c953924875005307d2a296768d8095adba51c14d
Khaled Amirat <khaled.amirat@yahoo.com>
2024-02-13 21:30:37 +0100

    adding endpoint specialties

--- api/index.py
+++ api/index.py
@@ -1,27 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 import json
+from supabase import create_client, Client
 
 app = Flask(__name__)
+
+url="https://hssqpmvmxjemxpnxlten.supabase.co"
+key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhzc3FwbXZteGplbXhwbnhsdGVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczODYzNDksImV4cCI6MjAyMjk2MjM0OX0.MXW1cXjLxD-lSRSRu4xV2mz7m1hJYvbwLoEtpg0c3nM"
+
+supabase: Client = create_client(url, key)
+
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-@app.route('/records.type')
-def get_record_types():
-    data=[
-    {'id':1,'name':'Medical Consultation'},
-    {'id':2,'name':'Analyse'},
-    ]
-    return json.dumps(data)
 
-@app.route('/doctors.list')
-def get_list_doctors():
-    data=[
-    {'id':1,'name':'Dr Amirat','address':'ABC'},
-    {'id':2,'name':'Dr Backouche','address':'ABCbac'},
-    ]
-    return json.dumps(data)
+@app.route('/specialties.get')
+def api_specialties_get():
+    response = supabase.table('specialties').select("*").execute()
+    return json.dumps(response.data)
+
 
 
 @app.route('/about')
