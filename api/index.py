21c11cb9fc0347c4d0b9a17e232f6d977630c7c1
Ines Amirat <inesamirat20@gmail.com>
2024-04-03 18:45:39 +0100

    kkk

--- api/index.py
+++ api/index.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 import json
 from supabase import create_client, Client
-
-
 from werkzeug.utils import secure_filename
 import os
 
@@ -48,39 +46,3 @@ def api_Relation_get():
 
 
 
-@app.route('/about')
-def about():
-    return 'About'
-
-
-# Assurez-vous que le dossier de destination existe ou créez-le
-UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-@app.route('/upload_images', methods=['POST'])
-def upload_images():
-    # Vérifiez si la partie 'image' est présente dans la requête
-    if 'image' not in request.files:
-        return 'No image part', 400
-    file = request.files['image']
-    
-    # Si l'utilisateur n'a pas sélectionné de fichier, le navigateur
-    # envoie une partie sans nom de fichier
-    if file.filename == '':
-        return 'No selected image', 400
-    if file:
-        # Sécurisez le nom du fichier pour éviter les failles de sécurité
-        filename = secure_filename(file.filename)
-        # Sauvegardez le fichier dans le dossier de destination
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        
-        # TODO: Ici, vous pourriez également uploader le fichier sur Supabase ou effectuer d'autres traitements
-        
-        return 'Image uploaded successfully', 200
-    else:
-        return 'Failed to upload image', 400
-    
-
-if __name__ == '__main__':
-    app.run(debug=True)
